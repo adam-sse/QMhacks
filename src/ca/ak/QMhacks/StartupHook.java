c971a4b17568176486bab07170b44853e6c4b6c0
adam-sse <adam@sse.uni-hildesheim.de>
2019-03-26 11:28:53 +0100

    Add logging to StartupHook

--- src/ca/ak/QMhacks/StartupHook.java
+++ src/ca/ak/QMhacks/StartupHook.java
@@ -1,5 +1,8 @@
 package ca.ak.QMhacks;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 /**
  * <p>
  * This class is loaded via <code>Class.forName()</code> when <code>eu.qualimaster.adaptation.platform.Main</code>
@@ -11,8 +14,14 @@ package ca.ak.QMhacks;
  */
 public class StartupHook {
     
+    public static final String LOGGING_PREFIX = "CA/AK: ";
+    
+    private static final Logger LOGGER = LogManager.getLogger(StartupHook.class);
+    
     static {
         // this block is executed when the infrastructure is up
+        
+        LOGGER.info(LOGGING_PREFIX + "StartupHook called");
     }
     
 }
